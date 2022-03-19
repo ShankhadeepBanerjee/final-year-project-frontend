01c2883085260a4ee89febedba4a720e7fbf0202
Shankhadeep <shankhadeep17.sb@gmail.com>
2022-03-19 22:01:45 +0530

    chore: initial setup for project with vite, tailwindcss, react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-[100vh] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex justify-center items-center">
+      <div className="text-3xl text-white text-center font-bold ">
+        Hello This is the frontend of our final year project
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
