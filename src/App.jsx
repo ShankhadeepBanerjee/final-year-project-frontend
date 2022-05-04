01c5eaa2cd5d87eb0fc505999d117c44239dc02a
Shankhadeep <shankhadeep17.sb@gmail.com>
2022-05-04 23:58:58 +0530

    feat: adds routs for add-candidate page and verify-candidate page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddCandidate from "./pages/AddCandidate";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import VerifyCandidate from "./pages/VerifyCandidate";
 
 
 
@@ -10,6 +12,8 @@ function App() {
       <Routes>
           <Route path="/">
             <Route index  element={<Home />} />
+            <Route path="/add-candidate"  element={<AddCandidate />} />
+            <Route path="/verify-candidate"  element={<VerifyCandidate />} />
             <Route path="*" element={<Error />} />
         </Route>
       </Routes>
---
+++ src/pages/AddCandidate.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddCandidate() {
+  return (
+    <div>AddCandidate</div>
+  )
+}
