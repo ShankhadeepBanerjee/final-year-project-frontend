7317a7de54f5ba3a36988acc9092a2c0445af732
Shankhadeep <shankhadeep17.sb@gmail.com>
2022-05-19 00:25:48 +0530

    feat: adds faceapi.js and basic setup of it

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import Error from "./pages/Error";
 import Home from "./pages/Home";
 import VerifyCandidate from "./pages/VerifyCandidate";
 
+import { createContext, useEffect, useState } from "react";
+import * as faceapi from "face-api.js";
 
-import { useEffect } from "react";
-import * as faceapi from "face-api.js" ;
+export const faceAPIContext = createContext();
 
 function App() {
+  const [modelsLoaded, setModelsLoaded] = useState(false);
   useEffect(() => {
-    (async ()=> {
-      const net = new faceapi.SsdMobilenetv1()
-      await net.loadFromUri("./model");
-      console.log(faceapi.nets.tinyFaceDetector);
-    })()
-  }, [])
-  
+    (async () => {
+      const net = new faceapi.SsdMobilenetv1();
+      await faceapi.loadSsdMobilenetv1Model("/models");
+      console.log(faceapi.nets.ssdMobilenetv1);
+      setModelsLoaded(true);
+    })();
+  }, []);
+
   return (
     <Router>
       <Header />
