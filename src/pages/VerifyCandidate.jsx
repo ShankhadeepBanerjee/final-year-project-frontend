cd67ef00198f753d87aa90b22209bf407881c6c1
Shankhadeep <shankhadeep17.sb@gmail.com>
2022-05-22 00:09:43 +0530

    feat(verify): adds qr reading functionality

--- src/pages/VerifyCandidate.jsx
+++ src/pages/VerifyCandidate.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import QrReader from "../utils/QrReader";
 
 export default function VerifyCandidate() {
+  const [candidateID, setCandidateID] = useState("");
+  console.log("Rendering 3");
   return (
-    <div>VerifyCandidate</div>
-  )
+    <div className="flex flex-col justify-center items-center">
+      <h1>Please Show Your QR in front of Camera</h1>
+      <h2>{`Your Candidate ID is: ${candidateID}`}</h2>
+      <QrReader
+        interval={3000}
+        onDecodeValue={(val) => setCandidateID(val)}
+        style={{ padding: "1rem" }}
+      />
+    </div>
+  );
 }
